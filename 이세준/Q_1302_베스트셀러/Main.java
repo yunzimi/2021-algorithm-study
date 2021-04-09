3d678df20b1daf8951c4283b9eba7074498a2a59
YimKeul <leesjun29@naver.com>
2021-04-09 17:37:41 +0900

    test

---
+++ 이세준/Q_1302_베스트셀러/Main.java
@@ -0,0 +1,43 @@
+package Q_1302_베스트셀러;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
+public class Main {
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		Map<String , Integer> map = new TreeMap<String, Integer>();
+		
+		for(int i =0; i< n ; i++) {
+			String input = br.readLine();
+			if(!map.containsKey(input)) {
+				map.put(input, 1);
+			}else {
+				map.put(input, map.get(input)+1);
+			}
+		}
+		
+//		for(String s : map.keySet()) {
+//			System.out.println(s + " : " + map.get(s));
+//		}
+		
+		//System.out.println(map.size());
+		
+		int max = 0;
+		String max_name = "";
+		for(Entry<String, Integer> m : map.entrySet()) {
+			if(max < m.getValue()) {
+				max = m.getValue();
+				max_name = m.getKey();
+			}
+		}
+		
+		//System.out.println(max);
+		System.out.println(max_name);
+	}
+}
