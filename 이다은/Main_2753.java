4279aabe99aea5618e5c69aa64d770e724759937
daeun0726 <82196822+daeun0726@users.noreply.github.com>
2021-04-20 10:14:42 +0900

    Add files via upload

---
+++ 이다은/Main_2753.java
@@ -0,0 +1,21 @@
+package github;
+
+import java.util.Scanner;
+
+public class Main_2753 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		 int a = sc.nextInt();
+		 
+		 if((a % 4 == 0 && a % 100 != 0) || a % 400 == 0) {
+			 System.out.println('1');
+		 }else {
+			 System.out.println('0');
+		 }
+		 System.out.println();
+	}
+
+}
