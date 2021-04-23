f1de050e090be175d3e4c9e6023570dfec19a27c
daeun0726 <82196822+daeun0726@users.noreply.github.com>
2021-04-23 18:11:09 +0900

    Add files via upload

---
+++ 이다은/Main_10950.java
@@ -0,0 +1,26 @@
+package github;
+
+import java.util.Scanner;
+
+
+public class Main_10950 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int[] b=new int[a];
+		
+		for(int i=0;i<a;i++){
+			int c = sc.nextInt();
+			int d = sc.nextInt();
+			b[i]=c+d;
+		}
+		
+		for(int j=0;j<a;j++){
+			System.out.println(b[j]);
+		}
+		
+	}
+}
