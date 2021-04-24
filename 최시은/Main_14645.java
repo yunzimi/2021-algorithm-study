28dd785ced8d13e1f6eb29a1e86000f32bdf978e
choisieun <82194112+choisieun@users.noreply.github.com>
2021-04-24 18:31:21 +0900

    Add files via upload

---
+++ 최시은/Main_14645.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main_14645 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		String[] str_su = input.split(" ");
+		int[] su = new int[2];
+		su[0] = Integer.parseInt(str_su[0]);
+		su[1] = Integer.parseInt(str_su[1]);
+		
+		for (int i = 0; i < su[0]; i++) {
+			String a = sc.nextLine();
+		}
+		System.out.println("비와이");
+
+	}
+
+}
