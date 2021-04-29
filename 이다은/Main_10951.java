cd6130fa28837f6000e7826917fa1e209ddcbd95
daeun0726 <82196822+daeun0726@users.noreply.github.com>
2021-04-29 12:43:03 +0900

    Add files via upload

---
+++ 이다은/Main_10951.java
@@ -0,0 +1,18 @@
+package github;
+
+import java.util.Scanner;
+
+public class Main_10951 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNextInt()){
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a+b);
+		}
+	}
+
+}
