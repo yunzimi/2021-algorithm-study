03b404f00065920bb7796883fcdac78a487e7837
daeun0726 <82196822+daeun0726@users.noreply.github.com>
2021-05-08 23:15:05 +0900

    Add files via upload

---
+++ 이다은/Main_1546.java
@@ -0,0 +1,39 @@
+package github;
+
+import java.util.Scanner;
+
+public class Main_1546 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		float[]b = new float[a];
+		
+		for(int i=0;i<a;i++){
+			b[i]=sc.nextInt();
+		}
+		
+		float max =b[0];
+		
+		for(int i=0; i<a;i++){
+				if(b[i]>max){
+					max = b[i];
+				}
+		}
+	
+		float[]c = new float[a];
+		
+		for(int i=0;i<a;i++){
+			c[i]=(b[i]/max*100);
+		}
+		
+		 float sum = 0;
+		
+		for(int i =0;i<a;i++){
+			sum=sum+c[i];
+		}
+		System.out.println(sum/a);
+	}
+}
