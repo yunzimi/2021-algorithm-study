b18748d425ce98c04c41391f940dd722ec23f12c
daeun0726 <82196822+daeun0726@users.noreply.github.com>
2021-05-11 13:48:09 +0900

    Add files via upload

---
+++ 이다은/Main_4344.java
@@ -0,0 +1,36 @@
+package github;
+
+import java.util.Scanner;
+
+public class Main_4344 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		 int a = sc.nextInt();
+	 
+		 for(int i=0;i<a;i++){
+			 int c=sc.nextInt();
+			 int [] score =new int [c];
+			 int sum=0;
+			 double aver = 0;
+			 int count = 0;
+			 for(int j=0;j<c;j++){
+				score[j]=sc.nextInt();
+				sum+=score[j];
+		     }
+			 aver=(sum/c);
+			 double rate =0;
+		     for(int k=0;k<c;k++){
+			 if(aver<score[k]){
+				 count++;
+		     }
+		     rate =(double)count/(double)c*100.0;
+		   }
+		    System.out.printf("%.3f", rate);
+	        System.out.println("%");
+      }
+   }
+}
+
