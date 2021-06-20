c8fdfc53b3da808505f447b18ccd94059a4657db
daeun0726 <82196822+daeun0726@users.noreply.github.com>
2021-06-20 17:41:36 +0900

    Add files via upload

---
+++ 이다은/Main_2941.java
@@ -0,0 +1,26 @@
+package github;
+
+import java.util.*;
+
+public class Main_2941 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+        
+        String a = sc.nextLine();
+        
+        a = a.replace("c=", "a");
+        a= a.replace("c-", "a");
+        a = a.replace("dz=", "a");
+        a = a.replace("d-", "a");
+        a= a.replace("lj", "a");
+        a = a.replace("nj", "a");
+        a = a.replace("s=", "a");
+        a= a.replace("z=", "a");
+        
+        System.out.println(a.length());
+     }
+}
+
+
