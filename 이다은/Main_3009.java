c3d2ceffc27e6d855bd1a844ee995157628be8cd
daeun0726 <82196822+daeun0726@users.noreply.github.com>
2021-07-08 23:07:25 +0900

    Add files via upload

---
+++ 이다은/Main_3009.java
@@ -0,0 +1,44 @@
+package github;
+
+import java.util.*;
+
+public class Main_3009 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+        
+        int x[] = new int[3];
+        int y[] = new int[3];
+        
+        for(int i=0; i<3; i++) {
+           x[i] = sc.nextInt();
+           y[i] = sc.nextInt();
+        }
+
+        if(x[0]==x[1]){
+        	if(y[0]==y[2]){
+        	  System.out.println(x[2]+" "+y[1]);
+        	}
+        	if(y[1]==y[2]){
+        	  System.out.println(x[2]+" "+y[0]);
+        	}
+        }
+        if(x[0]==x[2]){
+        	if(y[0]==y[1]){
+        	  System.out.println(x[1]+" "+y[2]);
+        	}
+        	if(y[2]==y[1]){
+        	  System.out.println(x[1]+" "+y[0]);
+        	}       	
+        }
+        if(x[1]==x[2]){
+        	if(y[1]==y[0]){
+        	  System.out.println(x[0]+" "+y[2]);
+        	}
+        	if(y[2]==y[0]){
+        	  System.out.println(x[0]+" "+y[1]);
+        	}	
+        }        
+    }        
+}
